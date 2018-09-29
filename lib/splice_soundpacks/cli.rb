c5f9e9983ec31d7d24e74bb804977049b9faf6a1
Harley Rayner <harleymteden@gmail.com>
2018-09-29 19:08:26 +0000

    built out CLI with fake functionality

--- lib/splice_soundpacks/cli.rb
+++ lib/splice_soundpacks/cli.rb
@@ -3,7 +3,25 @@
 class SpliceSoundpacks::CLI 
   
   def call 
-    puts "Today's Daily Deals:"
+    list_soundpacks
+    menu
   end 
+  
+  def list_soundpacks
+    puts "Today's Most Popular Soundpacks"
+    puts <<-DOC
+    1. lex luger - 808s and kicks - May 2016
+    2. KASHMR - dance anthems 2018 - March 2018
+    3. Aphex Twin - Psychadelic Drums - December 1999
+    DOC
+  end
+  
+  def menu 
+    puts "Enter the number of the soundpack you'd like to dive into"
+  end 
+  
+  
+  
+  
   
 end 
