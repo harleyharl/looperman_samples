da1a8d40e976e7ea2991fc4f3ea98ac8a98555a4
Harley Rayner <harleymteden@gmail.com>
2018-09-30 17:24:40 +0000

    added deal class

--- lib/splice_soundpacks/cli.rb
+++ lib/splice_soundpacks/cli.rb
@@ -5,6 +5,7 @@ class SpliceSoundpacks::CLI
   def call 
     list_soundpacks
     menu
+    goodbye
   end 
   
   def list_soundpacks
@@ -14,13 +15,14 @@ class SpliceSoundpacks::CLI
     2. KASHMR - dance anthems 2018 - March 2018
     3. Aphex Twin - Psychadelic Drums - December 1999
     DOC
+    @deal = SpliceSoundpacks::Soundpack.today
   end
   
   def menu 
-    puts "Enter the number of the soundpack you'd like to dive into"
+    input = nil 
     while input!= "exit"
+    puts "Enter the number of the soundpack you'd like to dive into or type list"
     input = gets.strip.downcase
-    
     case input 
     when "1"
       puts "More info on pack 1"
@@ -28,8 +30,15 @@ class SpliceSoundpacks::CLI
       puts "More info on pack 2"
     when "3"
       puts "More info on pack 3"
+    when "list"
+      list_soundpacks
+    else 
+      puts "not sure what you want, type list or exit"
     end 
   end 
+  end 
+  
+  def goodbye 
   end 
   
 
