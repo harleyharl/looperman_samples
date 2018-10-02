599a453839f9bbe1afef4137dbdb25b78ae615a2
Harley Rayner <harleymteden@gmail.com>
2018-10-02 00:30:53 +0000

    updated CLI controller

--- lib/splice_soundpacks/soundpack.rb
+++ lib/splice_soundpacks/soundpack.rb
@@ -2,13 +2,13 @@ class SpliceSoundpacks::Soundpack
   
   attr_accessor :name, :artist, :price, :url 
   
-  def self.today
-    #should return a bunch of instances of deals
-    #     puts <<-DOC
-    # 1. lex luger - 808s and kicks - May 2016
-    # 2. KASHMR - dance anthems 2018 - March 2018
-    # 3. Aphex Twin - Psychadelic Drums - December 1999
-    # DOC
+  def self.today 
+    #should scrape splice and return soundpacks based on that data
+    self.scrape_deals 
+  end 
+  
+  def self.scrape_soundpacks
+    
     soundpack_1 = self.new 
     soundpack_1.name = "808s and kicks"
     soundpack_1.artist = "lex luger" 
