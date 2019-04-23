3c61c7cb83eff9474528b64ead201f9b44282a70
harleyharl <harleymteden@gmail.com>
2019-04-24 00:49:40 +1200

    cleaned out redundant code

--- lib/looperman_samples/creator.rb
+++ lib/looperman_samples/creator.rb
@@ -1,25 +1,25 @@
 module LoopermanSamples
   class Creator
 
-  attr_accessor :name, :samples
+    attr_accessor :name, :samples
 
-  @@creators = []
+    @@creators = []
 
-  def self.all
-    @@creators
-  end
+    def self.all
+      @@creators
+    end
 
-  def initialize(name = nil)
-    @name = name
-    @samples = []
-    @@creators << self
-  end
+    def initialize(name = nil)
+      @name = name
+      @samples = []
+      @@creators << self
+    end
 
 
-  def add_sample(sample)
-    sample.creator = self unless sample.creator
-    self.samples << sample unless self.samples.include?(sample)
-  end
+    def add_sample(sample)
+      sample.creator = self unless sample.creator
+      self.samples << sample unless self.samples.include?(sample)
+    end
 
   end
 end
